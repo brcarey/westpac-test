c0d351b62ade8d5447ac8365528874a0ce4e339a
Brendan <brendan@currencyvue.com>
2017-11-12 10:24:40 +1100

    fix(add basic unit test)
    test the CompanyDetails component as a whole, assert on number of tiles, and that clicking a tile shows the details modal

--- src/CompanyDetails.test.tsx
+++ src/CompanyDetails.test.tsx
@@ -1,11 +1,27 @@
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import * as enzyme from 'enzyme';
 import CompanyDetails from './CompanyDetails';
 import Company from './Company';
+import * as Adapter from 'enzyme-adapter-react-16';
+
+enzyme.configure({ adapter: new Adapter() });
 
 const data:Company = require('./sample-data.json');
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<CompanyDetails company={data} />, div);
+describe('<CompanyDetails />', () => {
+	let wrapper: enzyme.ReactWrapper;
+    it('Should render the company details and employee grid', () => {
+		wrapper = enzyme.mount(<CompanyDetails company={data} />);
+		expect(wrapper.find('li').length).toEqual(12);
+	});
+
+	describe('When we select an employee tile', () => {
+		beforeEach(() => {
+			wrapper.find('li').first().simulate('click');
+		});
+
+		it('Should display employee detail modal', () => {
+			expect(wrapper.find('EmployeeDetail').length).toEqual(1);
+		});
+	});
 });
